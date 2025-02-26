c5d702f6b7916bf9e33d3f9dcbbd61cc9c98c23d
System Administrator <root@iMac-SINDO.local>
2025-02-26 15:22:47 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'after-schola',
+  webDir: 'dist'
+};
+
+export default config;
