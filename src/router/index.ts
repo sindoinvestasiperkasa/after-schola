3a75b2de031126d0155421b185f7d5559299d3c8
PROGRAMMER <sindoinvestasiperkasa@iMac-SINDO-2.local>
2025-02-27 11:14:41 +0700

    ONBOARDING, LOGIN, REGISTER: initial component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,56 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/onboarding",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: () => import('@/views/home/HomeView.vue')
+    path: "/home",
+    name: "Home",
+    component: () => import("@/views/home/HomeView.vue"),
   },
   {
-    path: '/hub',
-    name: 'Hub',
-    component: () => import('@/views/hub/HubView.vue')
+    path: "/onboarding",
+    name: "On Boarding",
+    component: () => import("@/views/OnBoardingPage.vue"),
   },
   {
-    path: '/myAchievments',
-    name: 'My Achievments',
-    component: () => import('@/views/myAchievments/MyAchievmentsView.vue')
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/LoginPage.vue"),
   },
   {
-    path: '/myCourse',
-    name: 'My Course',
-    component: () => import('@/views/myCourse/MyCourseView.vue')
+    path: "/register",
+    name: "Register",
+    component: () => import("@/views/RegisterPage.vue"),
   },
   {
-    path: '/profile',
-    name: 'Profile',
-    component: () => import('@/views/profile/ProfileView.vue')
+    path: "/hub",
+    name: "Hub",
+    component: () => import("@/views/hub/HubView.vue"),
   },
-]
+  {
+    path: "/myAchievments",
+    name: "My Achievments",
+    component: () => import("@/views/myAchievments/MyAchievmentsView.vue"),
+  },
+  {
+    path: "/myCourse",
+    name: "My Course",
+    component: () => import("@/views/myCourse/MyCourseView.vue"),
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import("@/views/profile/ProfileView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
